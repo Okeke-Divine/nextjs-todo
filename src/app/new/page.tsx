f82be47e6a4268862420fd90b0a1ee398948a7f1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 11:05:07 +0100

    Updated the add todo page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,12 @@
-export default function New(){
-    return <h1>New</h1>;
+export default function New() {
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+      <div>
+        
+      </div>
+    </>
+  );
 }
