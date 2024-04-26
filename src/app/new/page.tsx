3026c9c527b999fcfb1f838275f33fe0e3402ff0
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 11:10:54 +0100

    Updated the add page layout

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function New() {
   return (
     <>
@@ -5,7 +7,27 @@ export default function New() {
         <h1 className="text-2xl">New</h1>
       </header>
       <div>
-        
+        <form className="flex gap-2 flex-2" action="">
+          <input
+            name="title"
+            type="text"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          />
+          <div className="flex gap-1 justify-end">
+            <Link
+              href=".."
+              className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+            >
+              Cancel
+            </Link>
+            <button
+              type="submit"
+              className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+            >
+              Create
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
