3e005f723194e234b2a3b80c0da162313d0a7ca5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 10:54:49 +0100

    Updated the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import { prisma } from "@/db";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  // await prisma.todo.create({data: {title: "Test", complete: false}})
+  const todos = await prisma.todo.findMany();
+  console.log(todos);
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
-        <Link href="/new" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
+        <Link
+          href="/new"
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        >
+          New
+        </Link>
       </header>
-      <ul></ul>
+      <ul className="pl-4"></ul>
     </>
   );
 }
