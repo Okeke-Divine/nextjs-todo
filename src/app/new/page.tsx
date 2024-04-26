10e8970c8320007465372aa4aff2b8e037392a74
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 11:17:02 +0100

    Completed the create logic

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { prisma } from "@/db";
+import {redirect} from "next/navigation";
 
 async function createTodo(data: FormData) {
   "use server";
@@ -8,6 +9,7 @@ async function createTodo(data: FormData) {
     throw new Error("Invalid title")
   }
   await prisma.todo.create({data: {title, complete: false}})
+  redirect("/")
 }
 
 export default function New() {
