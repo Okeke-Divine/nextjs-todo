f16a2a10ff92113dcc75d9234928edb0d964c413
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-26 10:47:04 +0100

    Updated the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
-        <Link href="/new">New</Link>
+        <Link href="/new" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
       </header>
       <ul></ul>
     </>
