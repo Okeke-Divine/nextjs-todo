4ba690a468995103c48515ef9b05893c136b2cbd
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-27 08:51:42 +0100

    Updated the code

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import { prisma } from "@/db";
-import {redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 
 async function createTodo(data: FormData) {
-  "use server";
-  const title = data.get("title")?.valueOf;
-  if(typeof title !== "string" || title.length === 0){
-    throw new Error("Invalid title")
-  }
-  await prisma.todo.create({data: {title, complete: false}})
-  redirect("/")
+  "use server"
+    
+  const title = data.get('title');
+
+    if(typeof title !== "string" || title.length === 0){
+        throw new Error('Invalid title');
+    }
+
+  console.log(title);
 }
 
 export default function New() {
@@ -18,29 +20,27 @@ export default function New() {
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">New</h1>
       </header>
-      <div>
-        <form className="flex gap-2 flex-col" action={createTodo}>
-          <input
-            name="title"
-            type="text"
+      <form action={createTodo} className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        />
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
             className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-          />
-          <div className="flex gap-1 justify-end">
-            <Link
-              href=".."
-              className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-            >
-              Cancel
-            </Link>
-            <button
-              type="submit"
-              className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-            >
-              Create
-            </button>
-          </div>
-        </form>
-      </div>
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Create
+          </button>
+        </div>
+      </form>
     </>
   );
 }
